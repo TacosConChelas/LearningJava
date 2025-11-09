735fd8e87bb7f31578297f9de5b965edc6df5ee7
TacosConChelas <adriherandezor@gmail.com>
2025-11-08 22:21:02 -0600

    agregando cambios

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,9 @@
 public class HelloWorld {
+    public static void main(String[] args) {
+        if (args == null) {
+            System.out.println("Hello, World!");
+        } else {
+            System.out.println("Hello, " + args[0] + "!");
+        }
+    }
 }
