48c9bdb355a992962c7f0c14ad45b374cf628184
TacosConChelas <adriherandezor@gmail.com>
2025-11-10 12:16:05 -0600

    Reto completado

--- src/PlusFromStdin.java
+++ src/PlusFromStdin.java
@@ -1,9 +1,15 @@
+
+
 import java.io.*;
 import java.util.*;
 
 class PlusFromStdin{
-    static void main(String[] args) {
-        
-    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1 << 16);
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println("El resultado es: " + (a + b));
+    }
 }
