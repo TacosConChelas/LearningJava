78bb481ac668c89207fe40a6b356523c04a59f98
TacosConChelas <adriherandezor@gmail.com>
2025-11-19 19:43:09 -0600

    reto 1 terminado

--- src/Topic_4/ConditionalMethodLogic.java
+++ src/Topic_4/ConditionalMethodLogic.java
@@ -15,7 +15,25 @@ package Topic_4;
 
 public class ConditionalMethodLogic {
     public static void main(String[] args) {
-        
+        System.out.println(gradeLevel(55));  // Fail
+        System.out.println(gradeLevel(65));  // Pass
+        System.out.println(gradeLevel(85));  // Good
+        System.out.println(gradeLevel(95));  // Excellent
+        System.out.println(gradeLevel(75));  // Pass    
     }
-    
+    /*
+    @param score init: es le valor q se va a evaluar y dependiendo de este será lo que @return
+    regrese
+    @return String: "Fail", "Pass", "Good", "Excellent" dependiendo del valor de score
+     */
+    public static String gradeLevel(int score){
+        if (60 >= score && score <= 79){
+            return "Pass";
+        } else if (80 >= score && score <= 89){
+            return "Good";
+        } else if (score >= 90){
+            return "Excellent";
+        }
+        return "Fail";
+    }   
 }
