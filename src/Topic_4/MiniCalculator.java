0f362e1e64c5a0a0923b86bcc794f95a5a74f117
TacosConChelas <adriherandezor@gmail.com>
2025-11-19 22:59:39 -0600

    reto 5 terminado

--- src/Topic_4/MiniCalculator.java
+++ src/Topic_4/MiniCalculator.java
@@ -23,9 +23,40 @@ package Topic_4;
 public class MiniCalculator {
     public static void main(String[] args) {
         
-    }   
+    }
+    /*
+    @param x int: primer número a sumar
+    @param y int: segundo número a sumar
+    @return int: suma de x + y
+    */   
     public static int add(int x, int y) {
         return x + y;
     }
-    
+    /*
+    @param x int: dividendo
+    @param y int: divisor
+    @return int: resultado de la división entera x / y, o 0 si y es 0
+    */
+    public static int subtract(int x, int y){
+        return x - y;
+    }
+    /*
+    @param x int: primer número a multiplicar
+    @param y int: segundo número a multiplicar
+    @return int: producto de x * y
+    */
+    public static int multiply(int x, int y){
+        return x * y;
+    }    
+    /*
+    @param x int: dividendo
+    @param y int: divisor
+    @return float: resultado de la división x / y, o 0 si y es 0
+    */
+    public static float divide(int x, int y){
+        if (y == 0){
+            return 0;
+        }
+        return (float) x / y;
+    }
 }
