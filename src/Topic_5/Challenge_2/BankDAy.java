9466983301fb5508c47ea3976dd957d8296b547b
TacosConChelas <adriherandezor@gmail.com>
2025-11-20 01:00:37 -0600

    haciendo retos

---
+++ src/Topic_5/Challenge_2/BankDAy.java
@@ -0,0 +1,31 @@
+/*
+Challenge 2 — Object Behavior & State Change
+Instructions:
+    Create a class BankAccount with:
+
+    private ownerName (String)
+
+    private balance (double)
+Create:
+    constructor that sets ownerName and starts balance at 0
+
+    method deposit(double amount)
+
+    method withdraw(double amount)
+
+    if result would be negative → refuse the operation
+    
+    method getBalance()
+Task (main method):
+    Create 2 accounts
+    Perform different deposits and withdrawals
+    Print the account with the highest balance
+Constraint:
+    You may NOT use arrays or lists yet.
+    Just compare the two accounts manually.
+*/
+package Topic_5.Challenge_2;
+
+public class BankDAy {
+    
+}
